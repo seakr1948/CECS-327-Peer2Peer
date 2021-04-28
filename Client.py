715c732ddfafd57ff6d4b1c6439e5fd168430d6f
seakr1948 <48813264+seakr1948@users.noreply.github.com>
2021-04-28 13:58:24 -0700

    client code that connects to the server. Sends a message and server repeats it back

--- Client.py
+++ Client.py
@@ -1,9 +1,18 @@
 import socket
-import time
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('192.168.56.1', 8087))
+host = "DESKTOP-Q6J99TN"
+port = 5000
 
-clientsocket.send(b'a')
-time.sleep(0.05)
-clientsocket.close()
+mysocket = socket.socket()
+mysocket.connect((host, port))
+
+message = input(" -> ")
+
+while message != 'q':
+    mysocket.send(message.encode())
+    data = mysocket.recv(1024).decode()
+
+    print("recieved from server: " + data)
+    message = input(" -> ")
+
+mysocket.close()
