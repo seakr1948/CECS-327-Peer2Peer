521e008e71999d4807b36ed1eb7b865d16850240
rv-ben <ruben.bramasco@student.csulb.edu>
2021-04-28 23:10:13 -0700

    Fixes join network code

--- communication/dataRecievers.py
+++ communication/dataRecievers.py
@@ -5,13 +5,17 @@ def send_json(host, port, jsonObj):
 
     # Create a new socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as temp_socket:
-
+        
+        print("Host IP: " + host)
+        print("Port: " + str(port))
         # Bind Socket to the correct address and port
-        temp_socket.bind((host, port))
+        temp_socket.connect((host, port))
 
         # Deserialize JSON and sent it to the server
-        jsonString = json.dumps(jsonObj)
-        temp_socket.sendall(jsonString)
+        json_string = json.dumps(jsonObj)
+
+        json_to_bytes = json_string.encode('utf-8')
+        temp_socket.send(json_to_bytes)
 
         print("Sent this : " + str(jsonObj))
     
--- join_network_test.py
+++ join_network_test.py
@@ -6,12 +6,12 @@ from os import path
 if __name__ == "__main__":
     ip = socket.gethostbyname(socket.gethostname())
     port_number = 5555
+    print(ip)
     
     relative_path = "./test_folder"
 
     test_node = node.Node(relative_path, ip, port_number)
-    test_node.init_meta_file()
 
-    other_ip  = "192.168.122.1"
+    host_ip  = "192.168.1.86"
     network_key = 9999
     test_node.start_a_thread(test_node.request_join_network, args_= (host_ip, port_number, ip))
