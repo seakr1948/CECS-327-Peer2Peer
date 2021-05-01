245be4cdcd1883e7c01e81aa4877bf4c03f38e27
daramirez0803 <58645677+daramirez0803@users.noreply.github.com>
2021-04-30 19:55:06 -0700

    Changed FindPorts to work as a class

--- FindPorts.py
+++ FindPorts.py
@@ -3,49 +3,57 @@ import threading
 from queue import Queue
 import time
 
-printLock = threading.Lock()
-q = Queue()
-target = ['']
-openPorts = []
+class FindPorts:
+    def __init__(self):
+        self.target = ''
+        self.openPorts = []
+        self.q = Queue()
+        self.printLock = threading.Lock()
 
-def setTarget(ip):
-    target[0] = ip
+    #return the open ports
+    def get_open_ports(self):
+        return self.openPorts
 
-def pscan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        con = s.connect((target[0],port))
-        with printLock:
-            print('port',port,'is open!')
-        openPorts.append(port)
-        con.close()
-    except:
-        pass
+    #scans an individual port
+    def pscan(self,port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:    #try to connect to port, if able print to console and add it to current open ports
+            con = s.connect((self.target,port))
+            with self.printLock:
+                print('port',port,'is open!')
+            self.openPorts.append(port)
+            con.close()
+        except:
+            pass
+    
+    #function that the threads will be working on
+    def threader(self):
+        while True:
+            worker = self.q.get()
+            self.pscan(worker)
+            self.q.task_done()
 
-def threader():
-    while True:
-        worker = q.get()
-        pscan(worker)
-        q.task_done()
+    #function that will scan for ports on a parameter IP
+    def scan_for_ports(self, ip):
+        self.target = ip
+        self.openPorts = [] #reset the openPorts list to be blank to not interfere with previous scan_for_ports
 
-def scanForPorts():
-    openPorts = []
-    for x in range (20):
-        t = threading.Thread(target = threader)
-        t.daemon = True
-        t.start()
+        for x in range (20): #number of threads
+            t = threading.Thread(target = self.threader)
+            t.daemon = True #thread will be killed when tasks are done
+            t.start()
 
-    for worker in range(8000,8101):
-        q.put(worker)
+        for worker in range(8000,8101): #range of ports we are scanning for
+            self.q.put(worker)
 
-    q.join()
+        self.q.join()   #wait for all threads to catch up
 
+def main():
+    myFinder = FindPorts()
+    myFinder.scan_for_ports('47.154.12.183')
+    ports = myFinder.get_open_ports()
 
-start = time.time()
-print("My IP")
-setTarget('47.154.12.183')
-scanForPorts()
-print('done')
-print("---%s seconds---" % (time.time() - start))
+    print(ports)
 
-print(openPorts)
+if __name__ == "__main__":
+    main()
